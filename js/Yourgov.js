ed2f34749c59eafc4c099d6d38a63d529b7b971d
DylanB <1012477@schoolsnet.act.edu.au>
2019-05-06 15:13:06 +1000

    init

---
+++ js/Yourgov.js
@@ -0,0 +1,12 @@
+function checkPin() {
+    var x, text;
+    //Get the value of input field with id="numb"
+    x = document.getElementById("pin").value;
+    // If x is Not a Number or less than one or greater than 10
+    if (x == "1234") {
+        text = "Missile Launched! <p><img class=\"img-fluid\" src=\"images/missile.gif\"></p>";
+    } else {
+        text = "Incorrect Pin, Access Denied! <p><img class=\"img-fluid\" src=\"images/accessdenied.gif\"></p>";
+    }
+    document.getElementById("feedback").innerHTML = text;
+}
