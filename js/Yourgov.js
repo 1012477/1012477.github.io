b0af4fe3d85736eeeb8262383dc3f4a106336b98
DylanB <1012477@schoolsnet.act.edu.au>
2019-05-22 12:30:54 +1000

    init

--- js/Yourgov.js
+++ js/Yourgov.js
@@ -3,7 +3,7 @@ function checkPin() {
     //Get the value of input field with id="numb"
     x = document.getElementById("pin").value;
     // If x is Not a Number or less than one or greater than 10
-    if (x == "1234") {
+    if (x == "1674") {
         text = "Missile Launched! <p><img class=\"img-fluid\" src=\"images/missile.gif\"></p>";
     } else {
         text = "Incorrect Pin, Access Denied! <p><img class=\"img-fluid\" src=\"images/accessdenied.gif\"></p>";
